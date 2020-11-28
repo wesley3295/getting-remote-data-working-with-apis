7da8a733079d69012337fbc6a3e591f6ea0bf5d5
Wesley <wspencer3295@yahoo.com>
2020-11-28 14:39:55 -0700

    commit tree 4c16ace2f57ec126abe914d5a5f72f2c74f0e33d

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -11,8 +11,17 @@ class GetPrograms
     response = Net::HTTP.get_response(uri)
     response.body
   end
+  
+  def program_school
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]
+    end
+  end
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
